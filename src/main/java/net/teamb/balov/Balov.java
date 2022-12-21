158940437b754484249e100f5bf0bcb709e84834
PlezDO <rmdylanownby@gmail.com>
2022-12-20 20:06:07 -0500

    Modified template so modding can begin

---
+++ src/main/java/net/teamb/balov/Balov.java
@@ -0,0 +1,18 @@
+package net.teamb.balov;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Balov implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final String modID = "balov";
+	public static final Logger LOGGER = LoggerFactory.getLogger(modID);
+
+	@Override
+	public void onInitialize() {
+		
+	}
+}
---
+++ src/main/java/net/teamb/balov/BalovClient.java
@@ -0,0 +1,9 @@
+package net.teamb.balov;
+import net.fabricmc.api.ClientModInitializer;
+
+public class BalovClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
